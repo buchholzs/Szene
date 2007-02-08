030a67190acfb95691a5c9a2a11e633362d8ede2
buchholzs <buchholzs@6446e83c-a70f-0410-a786-9c36226ba29d>
2007-02-08 12:52:49 +0000

    git-svn-id: https://svn.code.sf.net/p/plushplayer/code/trunk@82 6446e83c-a70f-0410-a786-9c36226ba29d

--- Scene/src/Controller.cpp
+++ Scene/src/Controller.cpp
@@ -6,9 +6,18 @@
 
 #include "Controller.h"
 
+#include "depui/depui.h"
 #include "Scene.h"
+#include "SceneDesktop.h"
+#ifdef WIN32
+#include <direct.h>
+#else
+#include <unistd.h>
+#endif
 
 namespace scene {
+
+static void *fileOpenOKSelectedHandler(struct MxObject * object, const MxEvent * const event);
 
 // ------------------------------------------------------------
 Controller::Controller (MoveMode *moveMode, Scene *scene) :
@@ -23,11 +32,65 @@ Controller::~Controller ()
 }
 
 // ------------------------------------------------------------
-void Controller::zoom (const float factor)
+void Controller::loadScene (SceneDesktop *desktop)
 {
-	// PRESERVE:BEGIN
-	// Insert your preservable code here...
-	// PRESERVE:END
+  /* text for filename */
+  MxStatictextArgs textargs;
+  MxArgsInit(&textargs);
+  textargs.caption = (const char *)MxMalloc(MX_MAX_PATH);
+  textargs.len = 0;
+  textargs.ownscaption = 1;
+  MxStatictext *okSel = MxStatictextNew(&desktop->base.object, 0, 0, 0, 0, &textargs);
+  okSel->base.object.handler = fileOpenOKSelectedHandler;
+  MxHide(&okSel->base.object, MxTrue);
+
+  /* Setup the file selector arguments */
+  MxFileselectorArgs args;
+
+  MxArgsInit(&args);
+  args.file = (char *)textargs.caption;
+  strcpy(args.file, "");
+  args.pattern = "*.SCX;*.scx";
+  args.attrib = FA_RDONLY | FA_DIREC;
+  args.window.caption = "Load scene";
+
+  /* Start the file selector */
+  MxFileselector *fs = MxFileselectorStart(&args, &desktop->base.object, &okSel->base.object);
+  return;
 }
 
-} // scene
+// File | Open Filedialog OK selected
+static void *fileOpenOKSelectedHandler(struct MxObject * object, const MxEvent * const event) {
+  MxStatictext *okSel	= (MxStatictext *)object;
+  SceneDesktop *desktop = (SceneDesktop *)MxParent(&okSel->base.object);
+  switch (event->type) {
+  case MxEventOk: 
+	  {
+		// change dir
+#ifdef WIN32
+		char *lbs = strrchr(okSel->caption, '/');
+		if (lbs != NULL) {
+			  *lbs='\0';
+		}
+		char *newDir= lbs == NULL ? NULL : strdup(okSel->caption);
+		const char *fileName = lbs == NULL ? okSel->caption : ++lbs;
+#else
+		char *newDir = dirname(okSel->caption);
+		const char *fileName = basename(okSel->caption);
+#endif
+		if (newDir != NULL) {
+		  chdir(newDir);
+		  free(newDir);
+		}
+		loadScene(desktop, fileName);
+		MxDestroy(object);	// destroy okSel
+	  }
+	break;
+  default:
+	return MxStatictextHandler(object, event);
+  }
+  return 0;
+}
+
+// ------------------------------------------------------------
+} // Controller
