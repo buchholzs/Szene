5efecc5b8f3262130176c48bf2ff5d657a044288
buchholzs <buchholzs@6446e83c-a70f-0410-a786-9c36226ba29d>
2007-02-12 11:33:47 +0000

    git-svn-id: https://svn.code.sf.net/p/plushplayer/code/trunk@93 6446e83c-a70f-0410-a786-9c36226ba29d

--- Scene/src/HudRefreshCmd.cpp
+++ Scene/src/HudRefreshCmd.cpp
@@ -9,7 +9,7 @@
 #include "Hud.h"
 
 namespace scene {
-  const int HudRefreshCmd::refresh_ivl_hud = 200; // Refresh-Interval HUD in msec
+  const int HudRefreshCmd::refresh_ivl_hud = 1000; // Refresh-Interval HUD in msec
 
 // ------------------------------------------------------------
 // Konstruktor
@@ -18,36 +18,30 @@ HudRefreshCmd::HudRefreshCmd (Hud *hud, struct _GR_context *ctx) :
 	  elapsedTime_(0), hud_(hud), ctx_(ctx)
 {
  //hudctx_ = GrCreateContext(ctx->gc_xmax + 1,ctx->gc_ymax + 1, NULL, NULL);
- hudctx_ = GrCreateFrameContext(GR_frameRAM1, ctx->gc_xmax + 2 /* sonst array bound read in GrBitBlt1bpp */,ctx->gc_ymax + 1, NULL, NULL);
-
+ //hudctx_ = GrCreateFrameContext(GR_frameRAM1, ctx->gc_xmax + 2 /* sonst array bound read in GrBitBlt1bpp */,ctx->gc_ymax + 1, NULL, NULL);
 }
 // ------------------------------------------------------------
 // Destruktor
 //
 HudRefreshCmd::~HudRefreshCmd ()
 {
-	GrDestroyContext(hudctx_);
+	//GrDestroyContext(hudctx_);
 }
 
 void HudRefreshCmd::Execute (float timeDiff) {
 	elapsedTime_+=timeDiff;
 
-	const GrContext *oldctx = GrCurrentContext();
-	if (elapsedTime_>refresh_ivl_hud) {
+/*	if (elapsedTime_>refresh_ivl_hud) {
 		elapsedTime_=0;
-		GrSetContext(hudctx_);
-		GrClearContext( GrBlack() );
 		hud_->display(hudctx_);
-	}
+	} */
+	hud_->display(ctx_);
 
-  /* Put the bitmap into the screen */
-  GrSetContext(ctx_);
-  GrBitBlt1bpp(ctx_,0,0,hudctx_,0,0,hudctx_->gc_xmax,hudctx_->gc_ymax,
-	  hud_->getStatusColor(),GrNOCOLOR);
-/*	GrBitBlt(ctx_,0,0,hudctx_,0,0,hudctx_->gc_xmax,hudctx_->gc_ymax,
-		GrImageModeColor(GrBlack())); */
-
-  GrSetContext(oldctx);
+	/* Put the bitmap into the screen */
+//  GrBitBlt1bpp(ctx_,0,0,hudctx_,0,0,hudctx_->gc_xmax,hudctx_->gc_ymax,
+//	  hud_->getStatusColor(),GrNOCOLOR); 
+//	GrBitBlt(ctx_,0,0,hudctx_,0,0,hudctx_->gc_xmax,hudctx_->gc_ymax,
+//	GrImageModeColor(GrBlack()));
 }
 
 } // scene
