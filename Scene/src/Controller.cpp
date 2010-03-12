4791fbf31f5d12e526a4f04de6c05f31dc12226b
buchholzs <buchholzs@6446e83c-a70f-0410-a786-9c36226ba29d>
2010-03-12 23:21:36 +0000

    Displays a key help window
    git-svn-id: https://svn.code.sf.net/p/plushplayer/code/trunk@141 6446e83c-a70f-0410-a786-9c36226ba29d

--- Scene/src/Controller.cpp
+++ Scene/src/Controller.cpp
@@ -15,7 +15,60 @@
 namespace scene {
 
 static void *fileOpenOKSelectedHandler(struct MxObject * object, const MxEvent * const event);
+static void callback_exit(MxObject * menu, void *data, unsigned int selected);
 
+MxMenuDef helpmenu[] = {
+	 {"Exit", callback_exit, 0, 0, 0, 0, 0, 0}
+	 ,
+	 {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+// ------------------------------------------------------------
+// Callbacks
+// ------------------------------------------------------------
+static void callback_exit(MxObject * menu, void *data, unsigned int selected)
+{
+	 (void) data;
+	SceneDesktop *desktop = (SceneDesktop *)MxParent(menu->parent);
+	desktop->directDisplay = true;
+	MxEnqueueSimple(menu->parent, MxEventExit, 0);
+}
+
+// File | Open Filedialog OK selected
+static void *fileOpenOKSelectedHandler(struct MxObject * object, const MxEvent * const event) {
+  MxStatictext *okSel	= (MxStatictext *)object;
+  SceneDesktop *desktop = (SceneDesktop *)MxParent(&okSel->base.object);
+  switch (event->type) {
+  case MxEventOk: 
+	  {
+		// change dir
+#ifdef WIN32
+		char *lbs = strrchr((char *)okSel->caption, '/');
+		if (lbs != NULL) {
+			  *lbs='\0';
+		}
+		char *newDir= lbs == NULL ? NULL : strdup(okSel->caption);
+		const char *fileName = lbs == NULL ? okSel->caption : ++lbs;
+#else
+		char *newDir = dirname(okSel->caption);
+		const char *fileName = basename(okSel->caption);
+#endif
+		if (newDir != NULL) {
+		  chdir(newDir);
+		  free(newDir);
+		}
+		loadScene(desktop, fileName);
+		MxDestroy(object);	// destroy okSel
+	  }
+	break;
+  default:
+	return MxStatictextHandler(object, event);
+  }
+  return 0;
+}
+
+// ------------------------------------------------------------
+// Member functions
 // ------------------------------------------------------------
 Controller::Controller (MoveMode *moveMode, Scene *scene) :
 	moveMode_(moveMode),
@@ -61,44 +114,37 @@ void Controller::loadScene (SceneDesktop *desktop)
 // ------------------------------------------------------------
 void Controller::resizeFileSelector(MxFileselector *fs) {
   /* Change the window size to fit the desktop size */
-   MxObject *parent = MxParent(&fs->base.object);
+  MxObject *parent = MxParent(&fs->base.object);
   MxGeomSize(&fs->base.object, MxW(&fs->base.object), MxH(parent)/2);
   MxGeomPosition(&fs->base.object, fs->base.object.position.x1, MxH(parent)/4);
   MxEventSendSimple(&fs->base.object, MxEventGeomChanged);
   MxEnqueueRefresh(&fs->base.object, MxTrue);
 }
 
-// File | Open Filedialog OK selected
-static void *fileOpenOKSelectedHandler(struct MxObject * object, const MxEvent * const event) {
-  MxStatictext *okSel	= (MxStatictext *)object;
-  SceneDesktop *desktop = (SceneDesktop *)MxParent(&okSel->base.object);
-  switch (event->type) {
-  case MxEventOk: 
-	  {
-		// change dir
-#ifdef WIN32
-		char *lbs = strrchr(okSel->caption, '/');
-		if (lbs != NULL) {
-			  *lbs='\0';
-		}
-		char *newDir= lbs == NULL ? NULL : strdup(okSel->caption);
-		const char *fileName = lbs == NULL ? okSel->caption : ++lbs;
-#else
-		char *newDir = dirname(okSel->caption);
-		const char *fileName = basename(okSel->caption);
-#endif
-		if (newDir != NULL) {
-		  chdir(newDir);
-		  free(newDir);
-		}
-		loadScene(desktop, fileName);
-		MxDestroy(object);	// destroy okSel
-	  }
-	break;
-  default:
-	return MxStatictextHandler(object, event);
-  }
-  return 0;
+// ------------------------------------------------------------
+void Controller::showHelp(SceneDesktop *desktop) {
+	 MxEditorArgs editorargs;
+	 char * buffer = "    w        move forward\n"
+		 "    a        move left\n"
+		 "    s        move backward\n"
+		 "    d        move right\n"
+		 "    r        reload scene\n"
+		 "    F3       Load Scene File\n"
+		 "    F9       Set move mode to walk\n"
+		 "    F10      Set move mode to fly\n"
+		 ;
+
+	 /* Make an editor object without menu */
+	 MxArgsInit(&editorargs);
+	 editorargs.menu.listarea.def = helpmenu;
+	 editorargs.win.caption = "Key Help";
+
+	 MxEditor *editor = MxEditorNew(&desktop->base.object, desktop->base.object.position.x1, 
+		 desktop->base.object.position.y1, 
+		 desktop->base.object.position.x2, 
+		 desktop->base.object.position.y2, 
+		 &editorargs);
+	 MxEditareaSet(&editor->scrledit.editarea, buffer, MxFalse, MxTrue);
 }
 
 // ------------------------------------------------------------
