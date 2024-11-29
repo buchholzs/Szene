d7dc90fe4bc510906e86f333bfca9cd42463b407
buchholzs <steffen.buchholz@gmx.de>
2024-11-30 00:11:00 +0100

    removed duplicate free okSel->caption aka pathcopy

--- Scene/src/Controller.cpp
+++ Scene/src/Controller.cpp
@@ -68,7 +68,6 @@ static void *fileOpenOKSelectedHandler(struct MxObject * object, const MxEvent *
 #endif
 		if (newDir != NULL) {
 		  chdir(newDir);
-		  free(pathcopy);
 		}
 		desktop->controller->loadScene(fileName);
 	  }
